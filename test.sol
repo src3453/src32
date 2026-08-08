c32a35a5e4c9f78e9a5eae77ca24ef89e48781bb
src3453 <103661526+src3453@users.noreply.github.com>
2026-08-09 02:36:13 +0900

    refactor: コードの可読性向上のためのコメント追加と構造整理

--- test.sol
+++ test.sol
@@ -20,11 +20,11 @@ fn arevcp (ptr len ptr2) :
     local addr 0
     local addr_rev 0
     while
-        ptr2 i add >addr
-        ptr len add i sub 1 sub >addr_rev
-        addr_rev ldb addr stb
-        i 1 add >i
-        i len neq
+        ptr2 i add >addr # get writing addr
+        ptr len add i sub 1 sub >addr_rev # get reading addr
+        addr_rev ldb addr stb # xfer bytes addr_rev to addr
+        i 1 add >i # increment ptr
+        i len neq # continue until reach end
     end
 ;
 
@@ -33,51 +33,51 @@ fn tostr (num ptr ptr2) :
     local curval 0
     local isneg 0
     local rem 0
-    num >curval
-    num 0 eq if
-        0x30 ptr stb retn
+    num >curval # store input
+    num 0 eq if # if zero
+        0x30 ptr stb retn # print "0" and return
     else
-        num sgn >isneg
+        num sgn >isneg # store isneg if negative
         num sgn if
         else
-            curval neg >curval
+            curval neg >curval # if negative negate and store
         end
         while
-            curval 10 mod >rem
-            0x30 rem add ptr i add stb
-            i 1 add >i
-            curval 10 div >curval
-            curval 0 neq
+            curval 10 mod >rem # mod 10 and get remainder
+            0x30 rem add ptr i add stb # add to 0x30 ("0") and calculate digit ascii code (ex: "9" -> 0x39)
+            i 1 add >i # increment ptr
+            curval 10 div >curval # division by 10
+            curval 0 neq # if not zero then continue
         end
-        isneg if
+        isneg if # if is negative
         else
-            0x2d ptr i add stb
-            i 1 add >i
+            0x2d ptr i add stb # append "-"
+            i 1 add >i # increment ptr
         end
-        ptr i ptr2 arevcp
-        retn
+        ptr i ptr2 arevcp # reverse bytearray
+        retn # and return
     end
 ;
 
 !var i 1
 !var is_mod3or5 0
 while
-    0 >is_mod3or5
-    i 3 mod if
-        "Fizz" prn
-        1 >is_mod3or5
+    0 >is_mod3or5 # reset flag
+    i 3 mod if # if mod 3
+        "Fizz" prn # print fizz
+        1 >is_mod3or5 # store flag
+    end 
+    i 5 mod if # if mod 5
+        "Buzz" prn # print buzz
+        1 >is_mod3or5 # store flag
     end
-    i 5 mod if
-        "Buzz" prn
-        1 >is_mod3or5
+    is_mod3or5 if # else
+        i 0x30000 0x30100 tostr # conv num to str
+        0x30100 prn # print ptr
     end
-    is_mod3or5 if
-        i 0x30000 0x30100 tostr
-        0x30100 prn
-    end
-    i 1 add >i
-    0x0d putc
-    0x0a putc
-    i 101 neq
+    i 1 add >i # increment i
+    0x0d putc # put cr
+    0x0a putc # put lf
+    i 101 neq # if not 100 then continue
 end
 halt
