702bef5e80d0ea49554519629449bc51154bba3b
src3453 <103661526+src3453@users.noreply.github.com>
2026-08-11 22:52:19 +0900

    test

--- test.sol
+++ test.sol
@@ -1,28 +1,23 @@
 !const VRAM_BASE 0x10000000
-!const VRAM_SIZE 2400
+!const VRAM_SIZE 0x12c00
 
-!include "sol/solstd/solstd_prng.sol"
-
-fn step () :
+fn step (i) :
     local scan 0
     local ptr 0
     while
         scan >ptr
         ptr VRAM_BASE add >ptr
-        rand_lfsr ptr stb
-        0x3F ptr 0x1000 add stb
+        ptr i mod ptr stb
         scan 1 add >scan
         scan VRAM_SIZE lt
     end
     retn
 ;
 
-1 0x80030001 stb
-1 0x8003F003 stb
-
 !var c 0
 while
-    step
+    c step
+    c 1 add >c
     0
 end
 
