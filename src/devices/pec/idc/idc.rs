64f3600fce247b44089ad7a79b885470c3f09860
src3453 <103661526+src3453@users.noreply.github.com>
2026-08-23 00:38:57 +0900

    added idc

---
+++ src/devices/pec/idc/idc.rs
@@ -0,0 +1,4 @@
+// IDC (Integrated Drive Controller)
+// This module provides an emulation of the Integrated Drive Controller (IDC) used in the system.
+// It handles communication with storage devices and manages data transfers between the CPU and storage peripherals.
+// Also tells DMAC to perform DMA transfers to/from the storage devices.
